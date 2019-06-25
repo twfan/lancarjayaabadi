be695e94a0a5e40b8b81392151e6463b3b23b4f5
taufan erlangga <taufan.erlangga@gmail.com>
2019-06-25 14:31:33 +0700

    sql

---
+++ lancarjaya.sql
@@ -0,0 +1,219 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 25 Jun 2019 pada 09.30
+-- Versi server: 10.3.16-MariaDB
+-- Versi PHP: 7.1.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lancarjaya`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `uname` varchar(30) NOT NULL,
+  `pass` varchar(70) NOT NULL,
+  `foto` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `admin`
+--
+
+INSERT INTO `admin` (`id`, `uname`, `pass`, `foto`) VALUES
+(8, 'malasngoding', '069c546d1d97fd9648d8142b3e0fd3a3', 'text.png');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang`
+--
+
+CREATE TABLE `barang` (
+  `id` int(11) NOT NULL,
+  `nama` text NOT NULL,
+  `modal` int(11) NOT NULL,
+  `grosir` int(11) NOT NULL,
+  `semi` int(11) NOT NULL,
+  `ecer` int(11) NOT NULL,
+  `pkp1` int(11) NOT NULL,
+  `pkp2` int(11) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `sisa` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `barang`
+--
+
+INSERT INTO `barang` (`id`, `nama`, `modal`, `grosir`, `semi`, `ecer`, `pkp1`, `pkp2`, `jumlah`, `sisa`) VALUES
+(1104, '1103', 10000, 12500, 13500, 14500, 10870, 11595, 100, 100),
+(1105, '1101', 25000, 31250, 33750, 36250, 27175, 28988, 250, 250),
+(1106, '1104', 13000, 16250, 17550, 18850, 14131, 15074, 100, 100),
+(1107, '1105', 15000, 18750, 20250, 21750, 16305, 17393, 150, 150),
+(1108, '1106', 15000, 18750, 20250, 21750, 16305, 17393, 100, 100),
+(1109, '1-1707', 32000, 40000, 43200, 46400, 34784, 37104, 100, 100),
+(1110, '1-1605', 11300, 14125, 15255, 16385, 12284, 13103, 50, 50),
+(1111, '1-22330', 5000, 6250, 6750, 7250, 5435, 5798, 250, 250),
+(1112, '1-2051', 6000, 7500, 8100, 8700, 6522, 6957, 50, 50),
+(1113, '1009', 500, 625, 675, 725, 544, 580, 10, 10),
+(1114, '1-6075 T6L', 33000, 41250, 44550, 47850, 35871, 38264, 50, 50);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang_laku`
+--
+
+CREATE TABLE `barang_laku` (
+  `id` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `nama` text NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `total_harga` int(20) NOT NULL,
+  `laba` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `barang_laku`
+--
+
+INSERT INTO `barang_laku` (`id`, `tanggal`, `nama`, `jumlah`, `harga`, `total_harga`, `laba`) VALUES
+(46, '2015-02-01', 'roti unibis', 2, 6000, 12000, 2000),
+(47, '2015-02-02', 'makkkanan', 7, 12000, 84000, 70000),
+(48, '2015-02-02', 'dji sam soe', 2, 15000, 30000, 2000),
+(49, '2015-02-03', 'makkkanan', 1, 12000, 12000, 10000),
+(50, '2015-02-01', 'tim tam', 2, 4000, 8000, 4000),
+(51, '2015-02-02', 'mild', 2, 17000, 34000, 4000),
+(52, '2015-02-03', 'magnum', 1, 18000, 18000, 6000),
+(53, '2015-02-06', 'dji sam soe', 2, 19000, 38000, 10000),
+(54, '2015-02-15', 'nu mild', 2, 19100, 38200, 10200),
+(55, '2015-02-27', 'roti unibis', 2, 8000, 16000, 6000),
+(56, '2015-02-19', 'roti unibis', 1, 7000, 7000, 2000),
+(57, '2015-01-14', 'roti unibis', 1, 7000, 7000, 2000),
+(58, '2015-02-01', 'pulpen', 1, 3000, 3000, 2000),
+(59, '2015-02-02', 'roti', 2, 3000, 6000, 2000),
+(63, '2016-01-22', 'tic tac', 8, 4000, 32000, 16000);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pengeluaran`
+--
+
+CREATE TABLE `pengeluaran` (
+  `id` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `keperluan` text NOT NULL,
+  `nama` text NOT NULL,
+  `jumlah` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `pengeluaran`
+--
+
+INSERT INTO `pengeluaran` (`id`, `tanggal`, `keperluan`, `nama`, `jumlah`) VALUES
+(1, '2015-02-06', 'de', 'diki', 1234);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `settings`
+--
+
+CREATE TABLE `settings` (
+  `grosir` tinyint(4) NOT NULL,
+  `semi_grosir` tinyint(4) NOT NULL,
+  `ecer` tinyint(4) NOT NULL,
+  `pkp1` tinyint(4) NOT NULL,
+  `pkp2` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `settings`
+--
+
+INSERT INTO `settings` (`grosir`, `semi_grosir`, `ecer`, `pkp1`, `pkp2`) VALUES
+(25, 35, 45, 6, 11);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `barang_laku`
+--
+ALTER TABLE `barang_laku`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `pengeluaran`
+--
+ALTER TABLE `pengeluaran`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1115;
+
+--
+-- AUTO_INCREMENT untuk tabel `barang_laku`
+--
+ALTER TABLE `barang_laku`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
+
+--
+-- AUTO_INCREMENT untuk tabel `pengeluaran`
+--
+ALTER TABLE `pengeluaran`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
