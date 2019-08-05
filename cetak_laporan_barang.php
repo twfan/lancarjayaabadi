1741f05b5462427bab364456dd51e1bb5de39acc
taufan erlangga <taufan.erlangga@gmail.com>
2019-08-06 00:54:39 +0700

    fix image laporan

--- cetak_laporan_barang.php
+++ cetak_laporan_barang.php
@@ -8,7 +8,7 @@ $pdf->SetMargins(2,1,1);
 $pdf->AliasNbPages();
 $pdf->AddPage();
 $pdf->SetFont('Times','B',11);
-$pdf->Image('./assets/ljaBlankLogo.png',1,1,3,2);
+$pdf->Image('./assets/logoLJA.jpeg',1,1,3,2);
 $pdf->SetX(4);            
 $pdf->MultiCell(19.5,0.5,'LANCAR JAYA ABADI',0,'L');
 $pdf->SetX(4);
